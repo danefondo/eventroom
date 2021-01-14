9a9606d1071ab491edac9e0a20c4a3714d2d17fb
robertfoenix <38993572+robertfoenix@users.noreply.github.com>
2021-01-14 11:50:16 +0200

    Update constants.js

--- client/src/constants.js
+++ client/src/constants.js
@@ -2,8 +2,3 @@ export const BASE_PATH =
   process.env.NODE_ENV === "development"
     ? "http://localhost:3000"
     : window.location.origin;
-
-export const VUE_APP_COFOCUS =
-  process.env.VUE_APP_COFOCUS === "OHMYTHISISCOFOCUSYES"
-    ? process.env.VUE_APP_COFOCUS
-    : "OHMYTHISISCOFOCUSYES";
